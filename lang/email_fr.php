74e0f8c6540587089b4d479104bdc6b87b07e015
christophe le drean <christophe@rouge-rose.net>
2018-06-17 19:30:59 +0200

    Notifications
    Adaptation du texte des messages

--- lang/email_fr.php
+++ lang/email_fr.php
@@ -6,18 +6,35 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	
 	// C
 	'cher_chere_madame' => "Chère",
-	'cher_chere_monsieur' => "Cher", 
+	'cher_chere_monsieur' => "Cher",
+	
+	'confirmation_activation_numero_actuel' => "Votre abonnement est maintenant actif.
+
+Vous recevrez votre exemplaire de <em>@titre_numero_encours@</em>, le numéro que vous avez choisi pour débuter votre abonnement, dans quelques jours.
+
+Nous vous souhaitons par avance une bonne lecture.",
+
+	
+	'confirmation_activation_numero_prochain' => "Votre abonnement est maintenant actif.
+
+Vous recevrez votre exemplaire de <em>@titre_numero_prochain@</em>, le numéro que vous avez choisi pour débuter votre abonnement, dès qu'il sera disponible.
+
+Nous vous souhaitons par avance une bonne lecture.",
 	
 	// I
 	'il_elle_madame' => "elle",
 	'il_elle_monsieur' => "il",
+	
 	'info_cheque_envoyer' => 'Veuillez libeller votre chèque : 
 -* en euros ;
 -* à l\'ordre de « @ordre@ » ;
 -* d\'un montant de @montant@ ;
 -* compensable dans une agence bancaire située en France ;
 -* accompagné, au dos du chèque, de la référence "<b>Transaction numéro @transaction@</b>".',
+	
 	'info_cheque_envoyer_adresse' => 'Veuillez envoyer votre chèque à l\'adresse :',
+	
+	
 	'info_virement_etablir' => 'Libellé de votre virement : <b>Transaction numéro @transaction@</b>
 _ Montant : @montant@
 
@@ -27,6 +44,11 @@ Compte bancaire :
 @adressebanque@
 -* IBAN : @iban@
 -* BIC : @bic@',
+	
+	
+	'intro_cheque' => "Nous vous confirmons que nous avons bien enregistré votre commande numéro <b>@reference@</b>, détaillée ci-dessous. Vous souhaitez régler par chèque, votre commande sera donc validée après réception de votre réglement.",
+	
+	'intro_virement' => "Nous vous confirmons que nous avons bien enregistré votre commande numéro <b>@reference@</b>, détaillée ci-dessous. Vous souhaitez régler par virement, votre commande sera donc validée après réception de votre réglement."
 	
 	// P
 	'politesse_signature' => "Cordialement, <br /> Le comité de rédaction de Vacarme",
